7c719a7ef43e55b54d4a74690941ae3aa7e75366
Harshil Prajapti <hp5896958@gmail.com>
2025-12-11 16:53:35 +0530

    Update project files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -116,11 +116,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 
+STATIC_URL = '/static/'
+
+# Remove STATICFILES_DIRS if static folder exists in BASE_DIR
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_URL = '/static/'
-STATIC_ROOT = '/home/harshil962/Portfolio/staticfiles'
+
+STATIC_ROOT = '/home/harshil962/Portfolio/staticfiles/'
+
 
 
 MEDIA_URL = '/media/'
